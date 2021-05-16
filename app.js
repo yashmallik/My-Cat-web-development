1a0b4373ab9a89f5ba4a09fe6c0821b4278b1f11
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-16 22:04:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ pics = [
     "cat3.jpeg", //2
     "cat4.jpeg", //3
     "cat5.jpeg", //4
-    "cat6.jpeg" //5
-    //6
+    "cat6.jpeg"  //5
+    "cat7.jpg"  //6
+    "cat8.jpg"  //7
+    "cat9.jpg" //9
+    "cat10.png" //10
 ];
 var bnt = document.querySelector("button");
 var imge = document.querySelector("img")
 var count = 0;
 
 bnt = addEventListener("click", function() {
-    if (count === 6) {
+    if (count === 10) {
         count = 0;
     }
     imge.scr = pics[count];

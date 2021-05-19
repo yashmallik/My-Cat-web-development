c9033f01d6e110b7ffb1fea235859fec71d40680
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-19 12:32:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,20 +5,34 @@ pics = [
     "cat3.jpeg", //2
     "cat4.jpeg", //3
     "cat5.jpeg", //4
-    "cat6.jpeg"  //5
-    "cat7.jpg"  //6
-    "cat8.jpg"  //7
-    "cat9.jpg" //9
+    "cat6.jpeg",  //5
+    "cat7.jpg",  //6
+    "cat8.jpg",  //7
+    "cat9.jpg", //9
     "cat10.png" //10
 ];
-var bnt = document.querySelector("button");
-var imge = document.querySelector("img")
-var count = 0;
+var btn = document.querySelector("button")
+var img = document.querySelector("img")
+var counter = 1
 
-bnt = addEventListener("click", function() {
-    if (count === 10) {
-        count = 0;
-    }
-    imge.scr = pics[count];
-    count = count + 1;
-})
+btn.addEventListener("click", function(){
+	if(counter === 11){
+		counter = 0
+	}
+	img.src = pics[counter]
+	counter = counter + 1
+});
+
+
+var btn = document.querySelector("button")
+var img = document.querySelector("img")
+var counter = 1
+
+btn.addEventListener("click", function(){
+	if(counter === 11){
+		counter = 0
+	}
+	img.src = pics[counter]
+	counter = counter + 1
+});
+
